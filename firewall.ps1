034f1955da343115e1e804c4fa41ab1205168834
gbrmit1 <robert.mitchell@atradius.com>
2022-12-07 14:51:52 +0000

    add firewall

--- firewall.ps1
+++ firewall.ps1
@@ -2,8 +2,8 @@ $rg = 'RM-TEST-FW-RG'
 
 New-AzResourceGroup -Name $rg -Location northeurope
 
-#New-AzResourceGroupDeployment `
-#-Name 'RM-ARM-AZ700-Resource-Group' `
-#-ResourceGroupName $rg `
-#-TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.json `
-#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.parameters.json
+New-AzResourceGroupDeployment `
+-Name 'RM-ARM-TEST-FW-Resource-Group' `
+-ResourceGroupName $rg `
+-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.json `
+-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.parameters.json
