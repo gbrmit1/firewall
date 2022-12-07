fafd37635d4ff81007d51bd26e8f1000ec489eca
gbrmit1 <robert.mitchell@atradius.com>
2022-12-07 15:49:07 +0000

    edited to incude vnets

--- firewall.ps1
+++ firewall.ps1
@@ -7,3 +7,9 @@ New-AzResourceGroupDeployment `
 -ResourceGroupName $rg `
 -TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.json `
 -TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.parameters.json
+
+New-AzResourceGroupDeployment `
+-Name 'RM-ARM-TEST-FW-Resource-Group' `
+-ResourceGroupName $rg `
+-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.json `
+-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.parameters.json
