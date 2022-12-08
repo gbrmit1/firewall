ebd1e195aaa99ed40d6a44a6e56151be0e9dd821
gbrmit1 <robert.mitchell@atradius.com>
2022-12-08 22:22:20 +0000

    added resource to add routetable to subnet

--- firewall.ps1
+++ firewall.ps1
@@ -11,6 +11,12 @@ New-AzResourceGroup -Name $rg -Location northeurope
 New-AzResourceGroupDeployment `
 -Name 'RM-ARM-TEST-FW-Resource-Group' `
 -ResourceGroupName $rg `
+-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/route-table.json `
+-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/route-table.parameters.json
+
+New-AzResourceGroupDeployment `
+-Name 'RM-ARM-TEST-FW-Resource-Group' `
+-ResourceGroupName $rg `
 -TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.json `
 -TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.parameters.json
 
@@ -19,9 +25,3 @@ New-AzResourceGroupDeployment `
 #-ResourceGroupName $rg `
 #-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.json `
 #-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.parameters.json
-
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-TEST-FW-Resource-Group' `
--ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/route-table.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/route-table.parameters.json
