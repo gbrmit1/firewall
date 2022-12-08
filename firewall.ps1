f9b84de6b4f85afca159fe74183abdf37a2f0da5
gbrmit1 <robert.mitchell@atradius.com>
2022-12-08 14:21:04 +0000

    edited reference in route table

--- firewall.ps1
+++ firewall.ps1
@@ -2,11 +2,11 @@ $rg = 'RM-TEST-FW-RG'
 
 New-AzResourceGroup -Name $rg -Location northeurope
 
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-TEST-FW-Resource-Group' `
--ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.parameters.json
+#New-AzResourceGroupDeployment `
+#-Name 'RM-ARM-TEST-FW-Resource-Group' `
+#-ResourceGroupName $rg `
+#-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.json `
+#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.parameters.json
 
 New-AzResourceGroupDeployment `
 -Name 'RM-ARM-TEST-FW-Resource-Group' `
@@ -14,11 +14,11 @@ New-AzResourceGroupDeployment `
 -TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.json `
 -TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.parameters.json
 
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-TEST-FW-Resource-Group' `
--ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.parameters.json
+#New-AzResourceGroupDeployment `
+#-Name 'RM-ARM-TEST-FW-Resource-Group' `
+#-ResourceGroupName $rg `
+#-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.json `
+#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.parameters.json
 
 New-AzResourceGroupDeployment `
 -Name 'RM-ARM-TEST-FW-Resource-Group' `
