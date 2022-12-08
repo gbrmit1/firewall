6f8e1e7de6680c71f10c4fef3312440295bad96c
gbrmit1 <robert.mitchell@atradius.com>
2022-12-08 23:47:56 +0000

    edited ps1 file to add firewall rules

--- firewall.ps1
+++ firewall.ps1
@@ -5,6 +5,12 @@ New-AzResourceGroup -Name $rg -Location northeurope
 New-AzResourceGroupDeployment `
 -Name 'RM-ARM-TEST-FW-Resource-Group' `
 -ResourceGroupName $rg `
+-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/policies.json `
+-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/policies.parameters.json
+
+New-AzResourceGroupDeployment `
+-Name 'RM-ARM-TEST-FW-Resource-Group' `
+-ResourceGroupName $rg `
 -TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.json `
 -TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.parameters.json
 
