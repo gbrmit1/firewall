37bb8d206cf059a819839c252cb8da28f697b63f
gbrmit1 <robert.mitchell@atradius.com>
2022-12-09 20:07:07 +0000

    edited base config to remove rules

--- firewall.ps1
+++ firewall.ps1
@@ -2,11 +2,11 @@ $rg = 'RM-TEST-FW-RG'
 
 New-AzResourceGroup -Name $rg -Location northeurope
 
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-TEST-FW-Resource-Group' `
--ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.parameters.json
+#New-AzResourceGroupDeployment `
+#-Name 'RM-ARM-TEST-FW-Resource-Group' `
+#-ResourceGroupName $rg `
+#-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.json `
+#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.parameters.json
 
 New-AzResourceGroupDeployment `
 -Name 'RM-ARM-TEST-FW-Resource-Group' `
