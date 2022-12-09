c180902e2a6a8fa441c17c9dcae5e06841d6741e
gbrmit1 <robert.mitchell@atradius.com>
2022-12-09 10:41:59 +0000

    moved policies creation back to base template for firewall

--- firewall.ps1
+++ firewall.ps1
@@ -5,29 +5,29 @@ New-AzResourceGroup -Name $rg -Location northeurope
 New-AzResourceGroupDeployment `
 -Name 'RM-ARM-TEST-FW-Resource-Group' `
 -ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/policies.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/policies.parameters.json
-
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-TEST-FW-Resource-Group' `
--ResourceGroupName $rg `
 -TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.json `
 -TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/base.parameters.json
 
 New-AzResourceGroupDeployment `
 -Name 'RM-ARM-TEST-FW-Resource-Group' `
 -ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/route-table.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/route-table.parameters.json
+-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/policies.json `
+-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/policies.parameters.json
 
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-TEST-FW-Resource-Group' `
--ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.parameters.json
+#New-AzResourceGroupDeployment `
+#-Name 'RM-ARM-TEST-FW-Resource-Group' `
+#-ResourceGroupName $rg `
+#-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/route-table.json `
+#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/route-table.parameters.json
 
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-TEST-FW-Resource-Group' `
--ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.parameters.json
+#New-AzResourceGroupDeployment `
+#-Name 'RM-ARM-TEST-FW-Resource-Group' `
+#-ResourceGroupName $rg `
+#-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.json `
+#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/vnets.parameters.json
+
+#New-AzResourceGroupDeployment `
+#-Name 'RM-ARM-TEST-FW-Resource-Group' `
+#-ResourceGroupName $rg `
+#-TemplateUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.json `
+#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/firewall/main/server.parameters.json
